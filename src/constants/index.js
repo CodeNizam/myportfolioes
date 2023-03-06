603295971b7fbc27189ae9b1fc9e35cba730c058
CodeNizam <codenizam@gmail.com>
2023-03-06 06:31:42 -0600

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,253 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  dslogo,
+  tesla,
+  mandela,
+  shopify,
+  carrent,
+  code,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Graphic Designer",
+    icon: mobile,
+  },
+  {
+    title: "React Native Developer",
+    icon: backend,
+  },
+  {
+    title: "Author & Teacher",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full-Stack Web Developer",
+    company_name: "Freelancer",
+    icon: code,
+    iconBg: "#ffffff",
+    date: "September 2019 - Present",
+    points: [
+      "Developed several responsive websites with the MERN stack while employing UX/UI design best practices.",
+      "Built RESTful APIs and utilized languages like GraphQL to efficiently query data from MongoDB databases.",
+      "Acquired working knowledge of Agile and Scrum methodologies as well as best CI/CD practices.",
+      "Utilized knowledge of Git and version control to manage repositories and facilitate collaborative development.",
+    ],
+  },
+  {
+    title: "Web Designer/Graphic Designer",
+    company_name: "Freelancer",
+    icon: mandela,
+    iconBg: "#E6DEDD",
+    date: "Sep 2019 - Present",
+    points: [
+      "Applied a solid understanding of color theory and design principles to curate exceptional project assets.",
+      "Collaborated professionally with clients to create on-brand designs that met business objectives.",
+      "Designed aesthetic beauty product labels that optimized brand appeal, increasing revenue by 400% last year.",
+    ],
+  },
+  {
+    title: "Arabic Language & Theology Instructor",
+    company_name: "DarusSalam Foundation",
+    icon: dslogo,
+    iconBg: "#ffffff",
+    date: "August 2018 - Present",
+    points: [
+      "Advanced DarusSalam's $50M expansion project by representing them at various national Islamic conferences.",
+      "Created several paid college-level courses for Muslim professionals in various Islamic disciplines.",
+      "Tailored algorithmic solutions to optimize DarusSalam's flagship programs boosting enrollment by 300%.",
+      "Delivered various workshops and seminars to audiences of over 4,500 attendees.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
